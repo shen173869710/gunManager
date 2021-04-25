149198f9e6ec89d326ca90b2f1ad644b3b2c1b9c
173869710@qq.com <shen469758936>
2021-04-25 17:03:11 +0800

    logo 替换

--- app/src/main/java/com/auto/di/guan/manager/adapter/Tab2Adapter.java
+++ app/src/main/java/com/auto/di/guan/manager/adapter/Tab2Adapter.java
@@ -5,11 +5,8 @@ import com.auto.di.guan.manager.basemodel.model.respone.WateringRecord;
 import com.auto.di.guan.manager.utils.DateUtils;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.viewholder.BaseViewHolder;
-
 import java.util.List;
-
 public class Tab2Adapter extends BaseQuickAdapter<WateringRecord, BaseViewHolder> {
-
     public Tab2Adapter(List<WateringRecord> data) {
         super(R.layout.manager_tab_2_item, data);
     }
@@ -19,7 +16,6 @@ public class Tab2Adapter extends BaseQuickAdapter<WateringRecord, BaseViewHolder
         getData().addAll(data);
         notifyDataSetChanged();
     }
-
     @Override
     protected void convert(BaseViewHolder holder, WateringRecord data) {
         holder.setText(R.id.item_0, data.getProjectName()+"");
